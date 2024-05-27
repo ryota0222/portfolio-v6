fd9208deae61ca033338eff57bd2097510ade246
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-05-27 21:38:32 +0900

    feat: Add lineseed font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,36 @@
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useRouter } from "next/router";
 
-import { fontSans, fontMono } from "@/config/fonts";
 import "@/styles/globals.css";
+
+const lineSeedJP = localFont({
+  src: [
+    {
+      path: "../fonts/LINESeedJP_OTF_Th.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedJP_OTF_Rg.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedJP_OTF_Bd.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/LINESeedJP_OTF_Eb.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -13,13 +38,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider navigate={router.push}>
       <NextThemesProvider>
-        <Component {...pageProps} />
+        <div className={lineSeedJP.className}>
+          <Component {...pageProps} />
+        </div>
       </NextThemesProvider>
     </NextUIProvider>
   );
 }
-
-export const fonts = {
-  sans: fontSans.style.fontFamily,
-  mono: fontMono.style.fontFamily,
-};
