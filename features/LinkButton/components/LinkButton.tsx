1d9b44da54eea966c2b44e0d47f9469262756ffd
Matsumoto Ryota <awpjdmga1943@icloud.com>
2024-05-30 19:30:47 +0900

    feat: rename LinkButton

---
+++ features/LinkButton/components/LinkButton.tsx
@@ -0,0 +1,29 @@
+import { RightUpArrowIcon } from "@/cores/icons";
+import { Button } from "@nextui-org/button";
+import { PropsWithChildren, memo } from "react";
+
+interface Props {
+  href: string;
+  type: "bordered" | "solid";
+  isExternal?: boolean;
+}
+
+export const LinkButton = memo<PropsWithChildren<Props>>(
+  ({ href, type, isExternal, children }) => {
+    return (
+      <Button
+        variant={type}
+        radius="full"
+        endContent={<RightUpArrowIcon size={20} />}
+        className={`text-sm border py-4 px-6 h-auto ${
+          type === "solid" && "bg-zinc-800 text-white border-zinc-300"
+        }`}
+        target={isExternal ? "_blank" : undefined}
+        rel={isExternal ? "noopener noreferrer" : undefined}
+        href={href}
+      >
+        {children}
+      </Button>
+    );
+  }
+);
