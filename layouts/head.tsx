ca6df354b285535e4db9696b757ef1ac08ef354a
RyoTa <ryotanny@gmail.com>
2024-06-07 22:14:09 +0900

    feat: Update ogp

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -10,6 +10,10 @@ export const Head = () => {
       <meta key="title" content={siteConfig.name} property="og:title" />
       <meta content={siteConfig.description} property="og:description" />
       <meta content={siteConfig.description} name="description" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:locale" content="ja_JP" />
+      <meta property="og:site_name" content={siteConfig.name} />
+      <meta property="og:image" content="https://portfolio.ryotanny.com/mv.png" />
       <meta
         key="viewport"
         content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
