e300d6f9e34c74a82fe6fc2827cd5f3337a99001
RyoTa <ryotanny@gmail.com>
2024-06-09 16:53:33 +0900

    feat: Update sectionTitle

--- cores/SectionTitle/components/SectionTitle.tsx
+++ cores/SectionTitle/components/SectionTitle.tsx
@@ -10,9 +10,19 @@ export const SectionTitle = memo<PropsWithChildren<Props>>(({ index, children })
   return (
     <div className="flex flex-col gap-2 items-center">
       <span aria-hidden role="presentation">
-        {index === 1 && <Image alt="1" as={NextImage} height={23} src="/images/section/01.svg" width={27} />}
+        {index === 1 && (
+          <Image alt="1" as={NextImage} height={23} radius="none" src="/images/section/01.svg" width={27} />
+        )}
         {index === 2 && (
-          <Image alt="2" as={NextImage} height={23} loading="lazy" src="/images/section/02.svg" width={27} />
+          <Image
+            alt="2"
+            as={NextImage}
+            height={23}
+            loading="lazy"
+            radius="none"
+            src="/images/section/02.svg"
+            width={27}
+          />
         )}
       </span>
       <span aria-hidden className="w-[1px] h-8 bg-zinc-600 mt-4" role="presentation" />
