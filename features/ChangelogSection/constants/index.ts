4c16274c2c0e798d3700a72b23af25872259bb47
RyoTa <ryotanny@gmail.com>
2024-06-09 17:14:22 +0900

    feat: Update changelog

--- features/ChangelogSection/constants/index.ts
+++ features/ChangelogSection/constants/index.ts
@@ -2,6 +2,12 @@ import { IChangelogItem } from '../types';
 
 export const ChangelogData: IChangelogItem[] = [
   {
+    version: '1.0.1',
+    date: '2024-6-9',
+    title: 'SEOとAccessibilityのスコアを改善しました',
+    description: 'SEOを82から100に、Accessibilityを79から99に改善しました✨',
+  },
+  {
     version: '1.0.0',
     date: '2024-6-5',
     title: 'ポートフォリオを公開しました',
