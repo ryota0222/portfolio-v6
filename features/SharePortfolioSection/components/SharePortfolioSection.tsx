4bf02c46196219f08ab411201f6156b5cd37a3c5
RyoTa. <45546517+ryota0222@users.noreply.github.com>
2026-05-31 23:37:44 +0900

    feat: 共有セクションのアイコンにチルトホバーを追加し画像を更新

--- features/SharePortfolioSection/components/SharePortfolioSection.tsx
+++ features/SharePortfolioSection/components/SharePortfolioSection.tsx
@@ -18,7 +18,7 @@ export const SharePortfolioSection = memo<Props>(({ isLast }) => {
             className="sm:w-[100px] sm:h-[100px] w-[64px] h-[64px]"
             loading="lazy"
             radius="full"
-            src="/images/profile.webp"
+            src="/images/icon.png"
           />
         </div>
       )}
@@ -26,26 +26,42 @@ export const SharePortfolioSection = memo<Props>(({ isLast }) => {
         {isLast ? <>One last favor!&nbsp;Would you share your portfolio?</> : 'Would you like to share?'}
       </h2>
       <div className="flex sm:gap-24 gap-10 justify-center sm:mt-24 mt-16">
-        <Link isExternal href={`https://twitter.com/intent/tweet?url=${siteConfig.url}&text=${siteConfig.name}`}>
-          <Image isZoomed alt="Xのアイコン" className="w-10 sm:w-16" loading="lazy" src="/images/share/x.png" />
-        </Link>
-        <Link isExternal href={`https://www.linkedin.com/sharing/share-offsite/?url=${siteConfig.url}`}>
+        <Link
+          isExternal
+          className="group"
+          href={`https://twitter.com/intent/tweet?url=${siteConfig.url}&text=${siteConfig.name}`}
+        >
           <Image
-            isZoomed
+            alt="Xのアイコン"
+            className="w-10 sm:w-16 transition-transform duration-300 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:-rotate-[8deg] group-hover:-translate-y-1"
+            loading="lazy"
+            src="/images/share/x.png"
+          />
+        </Link>
+        <Link
+          isExternal
+          className="group"
+          href={`https://www.linkedin.com/sharing/share-offsite/?url=${siteConfig.url}`}
+        >
+          <Image
             alt="Linkedinのアイコン"
-            className="w-10 sm:w-16"
+            className="w-10 sm:w-16 transition-transform duration-300 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:-rotate-[8deg] group-hover:-translate-y-1"
             loading="lazy"
             src="/images/share/linkedin.png"
           />
         </Link>
-        <Link isExternal href={`https://line.me/R/msg/text/?${siteConfig.name}%20${siteConfig.url}`}>
-          <Image isZoomed alt="LINEのアイコン" className="w-10 sm:w-16" loading="lazy" src="/images/share/line.png" />
-        </Link>
-        <Link isExternal href={`https://www.facebook.com/sharer/sharer.php?u=${siteConfig.url}`}>
+        <Link isExternal className="group" href={`https://line.me/R/msg/text/?${siteConfig.name}%20${siteConfig.url}`}>
           <Image
-            isZoomed
+            alt="LINEのアイコン"
+            className="w-10 sm:w-16 transition-transform duration-300 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:-rotate-[8deg] group-hover:-translate-y-1"
+            loading="lazy"
+            src="/images/share/line.png"
+          />
+        </Link>
+        <Link isExternal className="group" href={`https://www.facebook.com/sharer/sharer.php?u=${siteConfig.url}`}>
+          <Image
             alt="Facebookのアイコン"
-            className="w-10 sm:w-16"
+            className="w-10 sm:w-16 transition-transform duration-300 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:-rotate-[8deg] group-hover:-translate-y-1"
             loading="lazy"
             src="/images/share/facebook.png"
           />
