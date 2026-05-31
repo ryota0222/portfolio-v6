77659aac831b13f93901505877b56d1e96cebff7
RyoTa. <45546517+ryota0222@users.noreply.github.com>
2026-05-31 23:39:12 +0900

    chore: HobbySectionの未使用Tシャツ節を削除
    コメントアウトされたまま残っていたT-shirtセクションを削除した。

--- features/HobbySection/components/HobbySection.tsx
+++ features/HobbySection/components/HobbySection.tsx
@@ -22,22 +22,6 @@ export const HobbySection = memo(() => {
         </div>
       </section>
       {/*
-       * T-shirts
-       */}
-      {/* <section className="sm:mt-16 mt-12 w-full">
-        <div className="container mx-auto max-w-5xl px-6 flex-grow">
-          <SectionSubTitle color="yellow">T-shirt</SectionSubTitle>
-          <p className="my-6">
-            On sale at UNIQLO&apos;s UTme!
-            <br />
-            It&apos;s UNIQLO clothing, so it&apos;s very comfortable to wear✨
-          </p>
-        </div>
-        <div className="mt-16 flex justify-center w-full">
-          <TShirtList />
-        </div>
-      </section> */}
-      {/*
        * Share
        */}
       <section className="container mx-auto max-w-5xl px-6 flex-grow mt-24 mb-16 sm:my-60">
